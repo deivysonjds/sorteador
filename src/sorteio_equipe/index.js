835606d934d7fe526f4edc7b62215aae2bb5db9b
Deivyson José da Silva <139878479+Deivyson1401@users.noreply.github.com>
2024-06-13 02:22:12 +0000

    corrigido funcao de sortear equipes

--- src/sorteio_equipe/index.js
+++ src/sorteio_equipe/index.js
@@ -25,8 +25,10 @@ function sortearEquipes(){
     for (const index in lista_Nomes) {
         do {
             index_sorteado = Math.floor(Math.random() * lista_Nomes.length)
+            console.log(`teste 0 - ${index_sorteado}`);
             find_index = lista_de_index_sorteados.find((element) => element == index_sorteado)
-        } while (find_index);
+            console.log(`teste 1 - ${find_index}`);
+        } while (find_index != undefined);
         lista_de_index_sorteados.push(index_sorteado)
     }
 
